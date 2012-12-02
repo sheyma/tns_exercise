88d4567f1049102157eaffd17f5518593919cf1d
Şeyma Bayrak <s.bayrak.89@hotmail.com>
2012-12-02 21:14:59 +0100

    ex8, complete assignment A

--- ex08/tns8.m
+++ ex08/tns8.m
@@ -1,4 +1,4 @@
-N = 1000;
+N = 100000;
 v = 30;  % Average spike rate (Hz)
 hist_steps = 100;
 
@@ -16,17 +16,35 @@ Dx = min(xi):dx:max(xi);
 
 % plot hist figures
 figure(1);
+subplot(1,2,1)
 hist(xi, Dx);
-figure(2);
+subplot(1,2,2)
 hist(ti, Dt);
 
 % sum up percentiles
+n_xi = histc(xi, Dx);
+n_ti = histc(ti, Dt);
+
 c_xi = cumsum(n_xi);
 c_ti = cumsum(n_ti);
 
-figure(3);
+figure(2);
+subplot(1,2,1)
 bar(Dx, c_xi);
-figure(4);
+subplot(1,2,2)
 bar(Dt, c_ti);
 
 
+figure(3);
+subplot(1,2,1)
+plot(Dx, c_xi/N);
+subplot(1,2,2);
+plot(Dt, c_ti/N);
+
+
+figure(4);
+subplot(1,2,1)
+plot(Dx, (n_xi/N)/dx)
+
+subplot(1,2,2)
+plot(Dt , (n_ti/N)/(dt))
